6768c255abf8504ee433369e3e1ae114485c2796
chencs <ichencs@163.com>
2017-09-26 11:53:16 +0800

    chencs

--- Vlpp/Vlpp.h
+++ Vlpp/Vlpp.h
@@ -11035,13 +11035,6 @@ namespace vl
 ***********************************************************************/
 #pragma once
 
-#ifdef _DEBUG
-#pragma comment(lib,"nbasicd.lib")
-#pragma message("Automatically linking with nbasicd debug lib")
-#else
-#pragma comment(lib,"nbasic.lib")
-#pragma message("Automatically linking with nbasic release lib")
-#endif
 
 
 
--- nbasic/nbasicInc.h
+++ nbasic/nbasicInc.h
@@ -1,12 +1,5 @@
 #pragma once
 
-// #ifdef _DEBUG
-// #pragma comment(lib,"nbasicd.lib")
-// #pragma message("Automatically linking with nbasicd debug lib")
-// #else
-// #pragma comment(lib,"nbasic.lib")
-// #pragma message("Automatically linking with nbasic release lib")
-// #endif
 
 #include "Basic.h"
 
